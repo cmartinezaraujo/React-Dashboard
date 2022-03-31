f715a383441dd6418af688e41da5676049abae6c
Cesar Martinez <cmartinezaraujo@ewu.edu>
2022-03-31 14:59:57 -0700

    -Added User component -Added UserList component -Added NewUser component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,16 +2,27 @@ import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './Pages/home/Home';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import UserList from './Pages/userList/UserList';
+import User from './Pages/user/User';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/users" element={<UserList/>}/>
+          <Route exact path="/user/:userId" element={<User/>}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- dashboard/src/dummyData.js
+++ dashboard/src/dummyData.js
@@ -55,3 +55,27 @@ export const userData = [
         "Active Users": 5100,
       }
   ];
+
+  export const userRows = [
+    { id: 1, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 2, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 3, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 4, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 5, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 6, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 7, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 8, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 9, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 10, username: "Jon Snow", avitar: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png', 
+    email: "jon@gmail.com", status: "active", transaction: "$120.00" }
+  ];
+  
